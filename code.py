36577d17124039fa7b87d114038b5e1b1f296b6f
alexgaarciia <al.garcia636@gmail.com>
2023-11-09 15:05:35 -0500

    Optimized entire structure (included a new function called "evaluate_models" that invokes all the classifiers and stores pertinent information)

--- code.py
+++ code.py
@@ -6,17 +6,10 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.neural_network import MLPClassifier
 
-# Initialize a dictionary to hold all performance metrics
-performance_metrics = {
-    'Base-DT': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
-    'Top-DT with best parameters': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
-    'Base-MLP': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
-    'Top-MLP with best parameters': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []}
-}
 
-
-# Function used to print information
+# Function used to compute metrics
 def compute_metrics(classifier, X_test, y_test):
+    """This is a function that, given a classifier and the test sets, returns a full metrics' report"""
     y_pred = classifier.predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     report = classification_report(y_test, y_pred, zero_division=0)
@@ -27,7 +20,9 @@ def compute_metrics(classifier, X_test, y_test):
     return cm, report, accuracy, macro_f1, weighted_f1
 
 
+# Functions to print information:
 def print_info(model_name, cm, report, accuracy, macro_f1, weighted_f1):
+    """This function has the main goal of printing information"""
     print("--------------------------------------------------\n")
     print(f"Model Description: {model_name}\n")
     print("Confusion Matrix:\n", cm)
@@ -38,6 +33,8 @@ def print_info(model_name, cm, report, accuracy, macro_f1, weighted_f1):
 
 
 def print_info2(model_name, metrics):
+    """This function is mainly used to print the average, variance and standard deviation of the accuracy,
+    macro_f1 and weighted_f1 of a classifier that are run multiple times"""
     # Compute avg, var, std
     accuracy_avg = np.mean(metrics['accuracies'])
     accuracy_var = np.var(metrics['accuracies'])
@@ -49,6 +46,7 @@ def print_info2(model_name, metrics):
     weighted_f1_var = np.var(metrics['weighted_f1s'])
     weighted_f1_std = np.std(metrics['weighted_f1s'])
 
+    # Print the information
     print(f"Performance summary for {model_name}:")
     print(f"Accuracy: Avg={accuracy_avg}, Var={accuracy_var}, Std={accuracy_std}")
     print(f"Macro F1: Avg={macro_f1_avg}, Var={macro_f1_var}, Std={macro_f1_std}")
@@ -118,3 +116,59 @@ def top_mlp(X, X_train, X_test, y_train, y_test):
 
     return top_mlp_classifier, X_test, y_test, 'Top-MLP with best parameters' + str(top_mlp_best_param)
 
+
+# Code to run the models a certain number of iterations
+def evaluate_models(X, X_train, X_test, y_train, y_test, num_iterations):
+    """The main goal of this function is invoking all the classifiers"""
+    # Declare a dictionary that will allow us to store useful information of each classifier
+    performance_metrics = {
+        'Base-DT': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
+        'Top-DT': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
+        'Base-MLP': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []},
+        'Top-MLP': {'accuracies': [], 'macro_f1s': [], 'weighted_f1s': []}
+    }
+
+    # Train/test the classifiers a specific number of times (num_iterations)
+    for _ in range(num_iterations):
+        # Call classifiers
+        base_dt_class = base_dt(X, X_train, X_test, y_train, y_test)
+        top_dt_class = top_dt(X, X_train, X_test, y_train, y_test)
+        base_mlp_class = base_mlp(X, X_train, X_test, y_train, y_test)
+        top_mlp_class = top_mlp(X, X_train, X_test, y_train, y_test)
+
+        # Compute metrics
+        base_dt_metrics = compute_metrics(base_dt_class[0], X_test, y_test)
+        top_dt_metrics = compute_metrics(top_dt_class[0], X_test, y_test)
+        base_mlp_metrics = compute_metrics(base_mlp_class[0], X_test, y_test)
+        top_mlp_metrics = compute_metrics(top_mlp_class[0], X_test, y_test)
+
+        # Print corresponding information
+        print_info(base_dt_class[3], base_dt_metrics[0], base_dt_metrics[1], base_dt_metrics[2], base_dt_metrics[3],
+                       base_dt_metrics[4])
+        print_info(top_dt_class[3], top_dt_metrics[0], top_dt_metrics[1], top_dt_metrics[2], top_dt_metrics[3],
+                       top_dt_metrics[4])
+        print_info(base_mlp_class[3], base_mlp_metrics[0], base_mlp_metrics[1], base_mlp_metrics[2],
+                       base_mlp_metrics[3], base_mlp_metrics[4])
+        print_info(top_mlp_class[3], top_mlp_metrics[0], top_mlp_metrics[1], top_mlp_metrics[2], top_mlp_metrics[3],
+                       top_mlp_metrics[4])
+
+        # In case the classifiers are trained more than once, we keep the performance of every single iteration
+        if num_iterations != 1:
+            # Store information
+            performance_metrics["Base-DT"]['accuracies'].append(base_dt_metrics[2])
+            performance_metrics["Base-DT"]['macro_f1s'].append(top_dt_metrics[3])
+            performance_metrics["Base-DT"]['weighted_f1s'].append(top_dt_metrics[4])
+
+            performance_metrics["Top-DT"]['accuracies'].append(base_dt_metrics[2])
+            performance_metrics["Top-DT"]['macro_f1s'].append(base_dt_metrics[2])
+            performance_metrics["Top-DT"]['weighted_f1s'].append(base_dt_metrics[4])
+
+            performance_metrics["Base-MLP"]['accuracies'].append(base_mlp_metrics[2])
+            performance_metrics["Base-MLP"]['macro_f1s'].append(base_mlp_metrics[3])
+            performance_metrics["Base-MLP"]['weighted_f1s'].append(base_mlp_metrics[4])
+
+            performance_metrics["Top-MLP"]['accuracies'].append(top_mlp_metrics[2])
+            performance_metrics["Top-MLP"]['macro_f1s'].append(top_mlp_metrics[3])
+            performance_metrics["Top-MLP"]['weighted_f1s'].append(top_mlp_metrics[4])
+
+    return performance_metrics
