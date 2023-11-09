e9b143b2d9299fc2e4fa9cd2a8e48e4c4ec4275d
alexgaarciia <al.garcia636@gmail.com>
2023-11-09 10:38:24 -0500

    Modified entire code: separated classifiers, added a function to compute the metrics of the classifier, and included two functions to print information (still need to work on efficiency and clean code)

--- code.py
+++ code.py
@@ -16,7 +16,7 @@ performance_metrics = {
 
 
 # Function used to print information
-def print_model_metrics(iteration, classifier, X_test, y_test, model_name):
+def compute_metrics(classifier, X_test, y_test):
     y_pred = classifier.predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     report = classification_report(y_test, y_pred, zero_division=0)
@@ -24,44 +24,42 @@ def print_model_metrics(iteration, classifier, X_test, y_test, model_name):
     macro_f1 = f1_score(y_test, y_pred, average='macro', zero_division=0)
     weighted_f1 = f1_score(y_test, y_pred, average='weighted', zero_division=0)
 
-    # Append the current scores to the respective lists in the dictionary
-    metrics = performance_metrics[model_name]
-    metrics['accuracies'].append(accuracy)
-    metrics['macro_f1s'].append(macro_f1)
-    metrics['weighted_f1s'].append(weighted_f1)
+    return cm, report, accuracy, macro_f1, weighted_f1
 
-    if iteration == 1:
-        print("--------------------------------------------------\n")
-        print(f"Model Description: {model_name}\n")
-        print("Confusion Matrix:\n", cm)
-        print("\nClassification Report:\n", report)
-        print(f"Accuracy: {accuracy:.4f}")
-        print(f"Macro-average F1: {macro_f1:.4f}")
-        print(f"Weighted-average F1: {weighted_f1:.4f}\n")
-    else:
-        for model_name, metrics in performance_metrics.items():
-            accuracy_avg = np.mean(metrics['accuracies'])
-            accuracy_var = np.var(metrics['accuracies'])
-            accuracy_std = np.std(metrics['accuracies'])
-            macro_f1_avg = np.mean(metrics['macro_f1s'])
-            macro_f1_var = np.var(metrics['macro_f1s'])
-            macro_f1_std = np.std(metrics['macro_f1s'])
-            weighted_f1_avg = np.mean(metrics['weighted_f1s'])
-            weighted_f1_var = np.var(metrics['weighted_f1s'])
-            weighted_f1_std = np.std(metrics['weighted_f1s'])
 
-            print(f"Performance summary for {model_name}:")
-            print(f"Accuracy: Avg={accuracy_avg}, Var={accuracy_var}, Std={accuracy_std}")
-            print(f"Macro F1: Avg={macro_f1_avg}, Var={macro_f1_var}, Std={macro_f1_std}")
-            print(f"Weighted F1: Avg={weighted_f1_avg}, Var={weighted_f1_var}, Std={weighted_f1_std}\n")
+def print_info(model_name, cm, report, accuracy, macro_f1, weighted_f1):
+    print("--------------------------------------------------\n")
+    print(f"Model Description: {model_name}\n")
+    print("Confusion Matrix:\n", cm)
+    print("\nClassification Report:\n", report)
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Macro-average F1: {macro_f1:.4f}")
+    print(f"Weighted-average F1: {weighted_f1:.4f}\n")
+
+
+def print_info2(model_name, metrics):
+    # Compute avg, var, std
+    accuracy_avg = np.mean(metrics['accuracies'])
+    accuracy_var = np.var(metrics['accuracies'])
+    accuracy_std = np.std(metrics['accuracies'])
+    macro_f1_avg = np.mean(metrics['macro_f1s'])
+    macro_f1_var = np.var(metrics['macro_f1s'])
+    macro_f1_std = np.std(metrics['macro_f1s'])
+    weighted_f1_avg = np.mean(metrics['weighted_f1s'])
+    weighted_f1_var = np.var(metrics['weighted_f1s'])
+    weighted_f1_std = np.std(metrics['weighted_f1s'])
+
+    print(f"Performance summary for {model_name}:")
+    print(f"Accuracy: Avg={accuracy_avg}, Var={accuracy_var}, Std={accuracy_std}")
+    print(f"Macro F1: Avg={macro_f1_avg}, Var={macro_f1_var}, Std={macro_f1_std}")
+    print(f"Weighted F1: Avg={weighted_f1_avg}, Var={weighted_f1_var}, Std={weighted_f1_std}\n")
 
 
 # Design classifiers
-def models_run(iteration, X, X_train, X_test, y_train, y_test):
+def base_dt(X, X_train, X_test, y_train, y_test):
     # (a) Base-DT
     base_dt_classifier = DecisionTreeClassifier(random_state=42)
     base_dt_classifier.fit(X_train, y_train)
-    print_model_metrics(iteration, base_dt_classifier, X_test, y_test, 'Base-DT')
 
     # Visualize the Decision Tree graphically
     plt.figure(figsize=(12, 8))
@@ -69,6 +67,10 @@ def models_run(iteration, X, X_train, X_test, y_train, y_test):
     plt.title("Base-DT Classifier")
     plt.show()
 
+    return base_dt_classifier, X_test, y_test, "Base-DT"
+
+
+def top_dt(X, X_train, X_test, y_train, y_test):
     # (b) Top_DT
     param_grid = {
         'criterion': ['gini', 'entropy'],
@@ -82,9 +84,6 @@ def models_run(iteration, X, X_train, X_test, y_train, y_test):
     top_dt_best_param = grid_search_DT.best_params_
     top_dt_classifier = grid_search_DT.best_estimator_
 
-    # print("The best parameters of Top-DT classification is:", top_dt_best_param)
-    print_model_metrics(iteration, top_dt_classifier, X_test, y_test, 'Top-DT with best parameters')
-
     # Visualize the Decision Tree graphically
     plt.figure(figsize=(12, 8))
     plot_tree(grid_search_DT.best_estimator_, filled=True, feature_names=X.columns,
@@ -92,11 +91,18 @@ def models_run(iteration, X, X_train, X_test, y_train, y_test):
     plt.title("Top-DT Classifier")
     plt.show()
 
+    return top_dt_classifier, X_test, y_test, 'Top-DT with best parameters' + str(top_dt_best_param)
+
+
+def base_mlp(X, X_train, X_test, y_train, y_test):
     # (c) Base-MLP
     base_MLP = MLPClassifier(hidden_layer_sizes=(100, 100), activation='logistic', solver='sgd')
     base_MLP.fit(X_train, y_train)
-    print_model_metrics(iteration, base_MLP, X_test, y_test, 'Base-MLP')
 
+    return base_MLP, X_test, y_test, 'Base-MLP'
+
+
+def top_mlp(X, X_train, X_test, y_train, y_test):
     # (d) Top_MLP
     param_grid_mlp = {
         'activation': ['logistic', 'tanh', 'relu'],
@@ -110,6 +116,5 @@ def models_run(iteration, X, X_train, X_test, y_train, y_test):
     top_mlp_best_param = grid_search_MLP.best_params_
     top_mlp_classifier = grid_search_MLP.best_estimator_
 
-    # print("The best parameters of Top-MLP classification is:", top_dt_best_param)
-    print_model_metrics(iteration, top_mlp_classifier, X_test, y_test, 'Top-MLP with best parameters')
+    return top_mlp_classifier, X_test, y_test, 'Top-MLP with best parameters' + str(top_mlp_best_param)
 
